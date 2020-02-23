d2dd2ccd91337cddf80df6ce2343892b428d5e4b
petermeissner <retep.meissner@gmail.com>
2020-02-23 12:19:20 +0100

    make ci work 1/x

--- R/test_method_call.R
+++ R/test_method_call.R
@@ -1,16 +1,16 @@
 #' test_method_call
 #'
-#' Example function that create an Java class instance and calls a method
-#' of the object jsut created.
+#' Example function that creates an Java class instance and calls a method
+#' of the object  that was created.
 #'
 #' @import rJava
 #' @export
 #'
 #' @examples
 #'
-#' .test_method_call()
+#' test_method_call()
 #'
-.test_method_call <-
+test_method_call <-
   function(){
     tmp <- rJava::.jnew("kafkaesquejars.App")
     tmp$test_method()
