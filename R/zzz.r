a594dfc7897bc5a056b587308fd493c4f50bb947
petermeissner <retep.meissner@gmail.com>
2020-02-23 12:54:52 +0100

    make ci work 4/x

--- R/zzz.r
+++ R/zzz.r
@@ -1,9 +1,22 @@
 .onLoad <- function(libname, pkgname) {
 
+  # let rJava find right Java version on its own
+  if ( grepl( "^i386", utils::sessionInfo()$platform) ){
+    Sys.setenv(
+      JAVA_HOME =
+        tail(
+          list.files("C:/Program Files (x86)/Java", full.names = TRUE),
+          1
+        )
+    )
+  } else {
+    Sys.setenv(JAVA_HOME = "")
+  }
+
   rJava::.jpackage(pkgname, jars = "*", lib.loc = libname)
   rJava::.jaddClassPath(
     list.files(
-      system.file("java", package = "kafkaesquejars")
+      system.file("java", package = pkgname)
     )
   )
 
