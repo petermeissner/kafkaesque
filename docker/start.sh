0fbdf516293c376479c8855b264e2c3bc1b81270
petermeissner <retep.meissner@gmail.com>
2021-01-17 12:42:57 +0100

    kafka test docker: polishing, robustness

--- docker/start.sh
+++ docker/start.sh
@@ -1,13 +1,15 @@
+#!/usr/bin/bash
+
 # Startup script for kafka
 
 # start zookeeper
-echo "\nstarting Zooleeper"
-nohup ./kafka/bin/zookeeper-server-start.sh ./kafka/config/zookeeper.properties &
+echo "starting Zookeeper"
+nohup ./kafka/bin/zookeeper-server-start.sh ./kafka/config/zookeeper.properties > /home/docker/nohup.out 2>&1 &
 
 
 # start kafka
-echo "\nstarting Kafka"
-nohup ./kafka/bin/kafka-server-start.sh ./kafka/config/server.properties &
+echo "starting Kafka"
+nohup ./kafka/bin/kafka-server-start.sh ./kafka/config/server.properties >> /home/docker/nohup.out 2>&1 &
 
 
 # waiting for zookeeper and kafka to finish startup
@@ -15,28 +17,33 @@ out="\rwaiting for Kafka to finish startup"
 while ! grep -e "started (kafka.server.KafkaServer)" nohup.out > /dev/null;
 do
     out=$out"."
-    if [ ${#out} -ge 50 ]; then 
-        out=out="\rwaiting "
-        sleep 0.1
-        echo -n $out
+    sleep 0.1
+    if [ ${#out} -ge 10 ]; then 
+        out="${out}."
+        echo -n "."
     fi
 done
+echo ""
 
 
 # adding testdata
-echo "\n... adding messages ... to topic test500000"
+echo "... adding messages ... to topic test500000"
 kafkacat -P -b localhost -t test500000 -l test_500_000.txt
 
-echo "\n... adding messages ... to topic test"
+echo "... adding messages ... to topic test"
 cat kafka_messages.txt | kafkacat -P -b localhost -t test
 
-echo "\n... adding messages ... to topic test2"
+echo "... adding messages ... to topic test2"
 cat kafka_messages.txt | kafkacat -P -b localhost -t test2 && \
 cat kafka_messages.txt | kafkacat -P -b localhost -t test2 && \
 cat kafka_messages.txt | kafkacat -P -b localhost -t test2
 echo "... done."
 
-echo "\n... starting script constantly adding messages to topic test3"
-nohup watch -n 0.1 "cat /proc/meminfo | grep MemFree | kafkacat -P -b localhost -t test3" > /dev/null &
+echo "... starting script constantly adding messages to topic test3"
+while /bin/true; do
+    cat /proc/meminfo | grep MemFree | kafkacat -P -b localhost -t test3
+    sleep 0.5
+done &
 
-
+echo "... sleeping and letting background tasks do their job"
+sleep infinity
